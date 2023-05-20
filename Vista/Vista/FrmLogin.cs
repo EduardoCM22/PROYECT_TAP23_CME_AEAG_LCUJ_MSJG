6c9ba3f1926af479be2192e99528fca8f9e584f4
EdwardCM <s21120182@alumnos.itsur.edu.mx>
2023-05-20 15:26:58 -0600

    Login base junto con Conexion(Datos) EmployeDAO(Datos) y Employee(Modelo)

---
+++ Vista/Vista/FrmLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+using Modelos;
+
+namespace Vista
+{
+    public partial class FrmLogin : MetroFramework.Forms.MetroForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            EmployeeDAO employeeDAO = new EmployeeDAO();
+            Employee emp = employeeDAO.login(txtUsuario.Text, txtContrasenia.Text);
+
+            if (emp != null)
+            {
+                MessageBox.Show("Bienvenido " + emp.FullName);
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos");
+            }
+        }
+    }
+}
